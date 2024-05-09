21a45f2d1c93c57c987a880eceb0ab5ee1d0c690
Pfm2024 <166004231+Pfm2024@users.noreply.github.com>
2024-05-09 20:37:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ inport streamlit as st
 inport numpy as np
 from PIL inport Image
 
-fichero = st.file.uploader("Subir imagen", type=["jpg","png","jpeg")
+fichero = st.file.uploader("Subir imagen", type=["jpg","png","jpeg"])
