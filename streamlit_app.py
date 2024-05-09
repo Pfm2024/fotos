3b97aef4e6887bb5b08ed0976fc15409c83c360f
Pfm2024 <166004231+Pfm2024@users.noreply.github.com>
2024-05-09 20:50:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ import numpy as np
 from PIL import Image
 
 fichero = st.file_uploader("Subir imagen", type=["jpg","png","jpeg"])
+
+if fichero is not None:
+  original = Image.open(fichero)
+  st.image(original)
